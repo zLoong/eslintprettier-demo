f35b483d6167ec8b4d01a143c9fa62d1548601c4
zloongjiang <zloongjiang@tencent.com>
2022-05-03 19:49:46 +0800

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,20 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ["plugin:react/recommended", "standard"],
+  extends: ['plugin:react/recommended', 'standard'],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
-  plugins: ["react"],
-  rules: {},
+  plugins: ['react'],
+  rules: {
+    // indent: ['error', 4],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always']
+  }
 };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+import fs from 'fs';
 const a = 1;
 
-console.log(a);
+const dd = 5;
+const zloong = 6;
+const cc = 7;
+// console.log(a);
+const v = 1;
+const b = 'ab';
+const c = 'dd';
+
+const ddd = new Set();
+
+const person = { name: 'Fred', age: 87 };
+Object.values(person); // ['Fred', 87]
+
+const add = async (a, b) => {
+  const c = a + 1;
+
+  return b + a + c;
+};
+
+const result = await add();
+
+const http = require('http');
+const gca = window.document.getElementById('cc');
+
+const tt = 1;
+if (tt == 1) {
+  console.log('oooo');
+}
